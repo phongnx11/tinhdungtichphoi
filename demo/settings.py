4efba2c110c0f9e94e74a7861c7a3f6f4ea7775c
phongnx11 <55699585+phongnx11@users.noreply.github.com>
2021-08-25 23:37:59 +0700

    Update settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -119,10 +119,10 @@ USE_L10N = True
 
 USE_TZ = True
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type
